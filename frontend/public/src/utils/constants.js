613845781b6a7e11f24cd2af64188788693daff0
akashBv6680 <153414012+akashBv6680@users.noreply.github.com>
2026-01-27 16:39:19 +0530

    Create constants.js

---
+++ frontend/public/src/utils/constants.js
@@ -0,0 +1,10 @@
+export const API_BASE_URL = "http://localhost:4000/api";
+
+export const ATS_SCORE_MAX = 100;
+
+export const ROLES = {
+  GUEST: "guest",
+  USER: "user",
+  ADMIN: "admin",
+  QA: "qa",
+};
