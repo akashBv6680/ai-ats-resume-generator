3550a18703a5244081b4ac004a01d45a4f63fc81
akashBv6680 <153414012+akashBv6680@users.noreply.github.com>
2026-01-27 17:03:06 +0530

    Create Loader.jsx

---
+++ frontend/public/src/components/Common/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div style={{ padding: "1rem", textAlign: "center" }}>
+      <span>Loading...</span>
+    </div>
+  );
+}
+
+export default Loader;
