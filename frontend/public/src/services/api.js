0841056c2ed8d258e047934d229b45578217ef1f
akashBv6680 <153414012+akashBv6680@users.noreply.github.com>
2026-01-27 16:40:59 +0530

    Create api.js

---
+++ frontend/public/src/services/api.js
@@ -0,0 +1,31 @@
+import { API_BASE_URL } from "../utils/constants";
+
+async function request(path, options = {}) {
+  const token = localStorage.getItem("token");
+  const headers = {
+    "Content-Type": "application/json",
+    ...(options.headers || {}),
+    ...(token ? { Authorization: `Bearer ${token}` } : {}),
+  };
+
+  const response = await fetch(`${API_BASE_URL}${path}`, {
+    ...options,
+    headers,
+  });
+
+  const data = await response.json().catch(() => null);
+
+  if (!response.ok) {
+    const message = data && data.message ? data.message : "Request failed";
+    throw new Error(message);
+  }
+
+  return data;
+}
+
+export default {
+  get: (path) => request(path),
+  post: (path, body) => request(path, { method: "POST", body: JSON.stringify(body) }),
+  put: (path, body) => request(path, { method: "PUT", body: JSON.stringify(body) }),
+  del: (path) => request(path, { method: "DELETE" }),
+};
